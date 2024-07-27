1131b4c75c4312dcab621b937fe7acf408b51dba
Alix Bourboulon <alix.bourboulon@gmail.com>
2024-07-27 12:14:14 +0200

    ex01 day4 done

---
+++ Day_04/ex01/in_out.py
@@ -0,0 +1,20 @@
+# Recoding some simple functions from the maths lib :
+
+# fonction qui retourne le carré de l'argument
+def square(x: int | float) -> int | float:
+    return (x * x)
+
+# fonction qui retourne l'argument puissance lui-même
+def pow(x: int | float) -> int | float:
+    return (x ** x)
+
+# fonction qui prend une fonction et un nbre en params, appel de la focntion interne
+# a chaque appel de l'objet
+def outer(x: int | float, function) -> object:
+    count = 0
+    def inner() -> float:
+        nonlocal count, x
+        count += 1
+        x = function(x)
+        return x
+    return inner
